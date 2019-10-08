c57120ec9cca2c33427660a709fd22a728a94795
Timothy Gross <mitcoding@gmail.com>
2019-10-08 11:57:41 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var debug = process.env.NODE_ENV !== "production";
+var webpack = require('webpack');
+var path = require('path');
+
+module.exports = {
+	context: path.join(__dirname, "src"),
+	devtool: debug ? "inline-sourcemap" : false,
+	entry: "./js/scripts.js",
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-react', '@babel/preset-env'],
+						plugins: ['react-html-attrs',  'transform-class-properties', ["@babel/plugin-proposal-decorators", { "legacy": true }], '@babel/plugin-proposal-function-bind']
+					}
+				}
+			}
+		]
+	},
+	output: {
+		path: path.resolve(__dirname, "target"),
+		publicPath: "/js/",
+		filename: "scripts.min.js"
+	},
+	plugins: debug ? [] : [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin({ mangle: false, sourcemap: false }),
+	],
+};
