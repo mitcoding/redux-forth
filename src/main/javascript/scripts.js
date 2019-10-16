00e92375595865149d5786439f195fdab637f873
Timothy Gross <mitcoding@gmail.com>
2019-10-16 11:41:13 -0700

    code clean up

--- src/main/javascript/scripts.js
+++ src/main/javascript/scripts.js
@@ -8,139 +8,338 @@ class WordNotFoundError extends Error {
 	}
 }
 
+class Word {
+	constructor(name, comment, command) {
+		this.name = name;
+		this.comment = comment;
+		this.command = command;
+	}
+}
+
+const defaultDictionary = {
+	"ABS" : new Word(
+		"ABS",
+		"(n -- -n)",
+		function abs(state) {
+			state.push(Math.abs(state.pop() ) );
+			return state;
+		}
+	),
+	"CONSTANT" : new Word("CONSTANT", "( -- w)"),
+	"DO" : new Word("(limit starting_value -- )"),
+	"DUP" : new Word(
+		"DUP",
+		"(n -- n n)",
+		function dup(state) {
+			state.push(state[state.length - 1]);
+			return state;
+		}
+	),
+	"DROP" : new Word(
+		"DROP",
+		"(n -- )",
+		function drop(state) {
+			return state.slice(0, state.length - 1);
+		}
+	),
+	"ELSE" : new Word("ELSE", "( -- )"),
+	"FALSE" : new Word(
+		"FALSE",
+		"( -- flag)",	
+		function add_false_flag(state) {
+			state.push(0);
+			return state;
+		}
+	),
+	"FORGET" : new  Word("FORGET", "(w -- )"),
+	"IF" : new Word("IF", "(flag -- )"),
+	"LOOP" : new Word("LOOP", "( -- )"),
+	"MAX" : new Word(
+		"MAX",
+		"(n1 n2 -- n3)",
+		function max(state) {
+			var 
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+
+			state.push(Math.max(topInt, nextInt) );
+			return state;
+		}
+	),
+	"MIN" : new Word(
+		"MIN",
+		"(n1 n2 -- n3)",
+		function min(state) {
+			var
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+
+			state.push(Math.min(topInt, nextInt) );
+			return state;
+		}
+	),
+	"MOD" : new Word(
+		"MOD",
+		"(n1 n2 -- n3)",
+		function mod(state) {
+			var 
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+
+			state.push(nextInt % topInt);
+			return state;
+		}
+	),
+	"NEGATE" : new Word(
+		"NEGATE",
+		"(n -- -n)",
+		function negate(state) {  
+			var topInt = state.pop();
+			state.push(topInt * -1);
+			return state;
+		}
+	),
+	"NOT" : new Word(
+		"NOT",
+		"(flag1 -- flag2)",
+		function not(state) {
+			var topInt = state.pop();
+			state.push(topInt === 0 ? -1 : 0);
+			return state;
+		}
+	),
+	"OVER" : new Word(
+		"OVER",
+		"(n1 n2 -- n1 n2 n1)",
+		function over(state) {
+			var nextInt = state[state.length - 2];
+			state.push(nextInt);
+			return state;
+		}
+	),
+	"PICK" : new Word(
+		"PICK",
+		"(n -- nth_number)",
+		function pick(state) {
+			var 
+				topInt = state[state.length - 1],
+				copyInt = state[state.length - (topInt + 1)]
+			;
+			
+			state.push(copyInt);
+			return state;
+		}
+	),
+	"ROT" : new Word(
+		"ROT",
+		"(n1 n2 n3 -- n2 n3 n1)",
+		function rot(state) {
+			var 
+				removeIndex = state.length - 3,
+				nextInt = state[removeIndex]
+			;
+			
+			state = state.filter(function(value, index, arr) { return index !== removeIndex });
+			state.push(nextInt);
+			return state;
+		}
+	),
+	"SWAP" : new Word(
+		"SWAP",
+		"(n1 n2 -- n2 n1)",
+		function swap(state) {
+			var 
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+			
+			state.push(topInt);
+			state.push(nextInt);
+			return state;
+		}
+	),
+	"THEN" : new Word("THEN", "( -- )"),
+	"TRUE" : new Word(
+		"TRUE",
+		"( -- flag)",
+		function add_true_flag(state) {
+			state.push(-1);
+			return state;
+		}
+	),
+	"." : new Word(
+		".",
+		"(n -- )",
+		function remove_top_number(state) {
+			var topInt = state.pop();
+			return state;
+		}
+	),
+	".S" : new Word(".S", "( -- )"),
+	";" : new Word(";", "( -- w)"),
+	":" : new Word(":", "( -- )"),
+	"(" : new Word("(", "( -- )"),
+	")" : new Word(")", "( -- )"),
+	"+" : new Word(
+		"+",
+		"(n1 n2 -- n1+n2)",
+		function add(state) {
+			var
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+			
+			state.push(nextInt + topInt);
+			return state;
+		}
+	),
+	"-" : new Word(
+		"-",
+		"(n1 n2 -- n1-n2)",
+		function minus(state) {
+			var 
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+
+			state.push(nextInt - topInt);
+			return state;
+		}
+	),
+	"*" : new Word(
+		"*",
+		"(n1 n2 -- n1*n2)",
+		function multiply(state) {
+			var
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+			
+			state.push(nextInt * topInt);
+			return state;
+		}
+	),
+	"/" : new Word(
+		"/",
+		"(n1 n2 -- n1/n2)",
+		function divide(state) {
+			var
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+
+			state.push(Math.floor(nextInt / topInt) );
+			return state;
+		}
+	),
+	"*/" : new Word(
+		"*/",
+		"(n1 n2 n3 -- (n1*n2)/n3)",
+		function multiply_divide(state) {
+			var topInt = state.pop();
+			state = defaultDictionary["*"].command(state);
+			state.push(topInt);
+			return defaultDictionary["/"].command(state);
+		}
+	),
+	"*/MOD" : new Word(
+		"*/MOD",
+		"(n1 n2 n3 -- (n1*n2)%n3=n4 (n1*n2)/n3=n5)",
+		function multiply_divide_mod(state) {
+			var 
+				topInt = state.pop(),
+				divState
+			;
+
+			state = defaultDictionary["*"].command(state);
+			state.push(topInt);
+			divState = defaultDictionary["/"].command([...state]);
+			state = defaultDictionary["MOD"].command(state);
+			state.push(divState.pop());
+			return state;
+		}
+	),
+	"<" : new Word(
+		"<",
+		"(n1 n2 -- (n1<n2)=flag)",
+		function less_than(state) {
+			var
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+			
+			state.push(nextInt < topInt ? -1 : 0);
+			return state;
+		}
+	),
+	">" : new Word(
+		">",
+		"(n1 n2 -- (n1>n2)=flag)",
+		function greater_than(state) {
+			var
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+			
+			state.push(nextInt > topInt ? -1 : 0);
+			return state;
+		}
+	),
+	"=" : new Word(
+		"=",
+		"(n1 n2 -- (n1=n2)=flag)",
+		function equals(state) {
+			var
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+			
+			state.push(nextInt === topInt ? -1 : 0);
+			return state;
+		}
+	),
+	"<>" : new Word(
+		"<>",
+		"(n1 n2 -- (n1!=n2)=flag)",
+		function not_equal(state) {
+			var
+				topInt = state.pop(),
+				nextInt = state.pop()
+			;
+			
+			state.push(nextInt !== topInt ? -1 : 0);
+			return state;
+		}
+	)
+};
+
 const numberStackReducer = function(state=[], action) {
 	state = [...state];
 	action = {...action};
 
-	if (isNumber(action.type)) {
+	let command = action.type.toUpperCase();
+	if (isNumber(command)) {
 		state.push(action.type * 1);
 		return state;
 	}
 
-	let specialDigitCommandMatch = isSpecialDigitCommand(action.type);
+	let specialDigitCommandMatch = isSpecialDigitCommand(command);
 	if (specialDigitCommandMatch && specialDigitCommandMatch.length === 3) {
-		let command = specialDigitCommandMatch[2];
+		command = specialDigitCommandMatch[2];
 		state.push(specialDigitCommandMatch[1] * 1);
 		return numberStackReducer(state, {...action, type: command});
 	}
 
-	switch(action.type) {
-		case "ABS" :
-			state.push(Math.abs(state.pop() ) );
-			return state;
-		case "CLEAR_INTEGER_STACK" :
-			return [];
-		case "DUP" :
-			state.push(state[state.length - 1]);
-			return state;
-		case "DROP" :
-			return state.slice(0, state.length - 1);
-		case "FALSE" :
-			state.push(0);
-			return state;
-		case "MAX" :
-			let topInt = state.pop();
-			let nextInt = state.pop();
-			state.push(Math.max(topInt, nextInt) );
-			return state;
-		case "MIN" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(Math.min(topInt, nextInt) );
-			return state;
-		case "MOD" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(nextInt % topInt);
-			return state;
-		case "NEGATE" : 
-			topInt = state.pop();
-			state.push(topInt * -1);
-			return state;
-		case "NOT" :
-			topInt = state.pop();
-			state.push(topInt === 0 ? -1 : 0);
-			return state;
-		case "OVER" :
-			nextInt = state[state.length - 2];
-			state.push(nextInt);
-			return state;
-		case "PICK" :
-			topInt = state[state.length - 1];
-			let copyInt = state[state.length - (topInt + 1)];
-			state.push(copyInt);
-			return state;
-		case "ROT" :
-			let removeIndex = state.length - 3;
-			nextInt = state[removeIndex];
-			state = state.filter(function(value, index, arr) { return index !== removeIndex });
-			state.push(nextInt);
-			return state;
-		case "SWAP" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(topInt);
-			state.push(nextInt);
-			return state;
-		case "TRUE" :
-			state.push(-1);
-			return state;
-		case "." :
-			topInt = state.pop();
-			return state;
-		case "+" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(nextInt + topInt);
-			return state;
-		case "-" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(nextInt - topInt);
-			return state;
-		case "*" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(nextInt * topInt);
-			return state;
-		case "/" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(Math.floor(nextInt / topInt) );
-			return state;
-		case "*/" :
-			topInt = state.pop();
-			state = numberStackReducer(state, {...action, type: '*'});
-			state.push(topInt);
-			return numberStackReducer(state, {...action, type: '/'});
-		case "*/MOD" :
-			topInt = state.pop();
-			state = numberStackReducer(state, {...action, type: '*'});
-			state.push(topInt);
-			let divState = numberStackReducer(state, {...action, type: '/'});
-			state = numberStackReducer(state, {...action, type: 'MOD'});
-			state.push(divState.pop());
-			return state;
-		case "<" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(nextInt < topInt ? -1 : 0);
-			return state;
-		case ">" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(nextInt > topInt ? -1 : 0);
-			return state;
-		case "=" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(nextInt === topInt ? -1 : 0);
-			return state;
-		case "<>" :
-			topInt = state.pop();
-			nextInt = state.pop();
-			state.push(nextInt !== topInt ? -1 : 0);
-			return state;
+	let word = defaultDictionary[command];
+	if (word instanceof Word) {
+		return word.command(state);
+	}
+
+	if (command === "CLEAR_INTEGER_STACK") {
+		return [];
 	}
 
 	return state;
@@ -152,7 +351,7 @@ const dictionaryReducer = function(state={stack: []}, action) {
 	action = {...action};
 	switch(action.type) {
 		case "CREATE_NEW_COMMAND" :
-			
+
 			action.payload.command = action.payload.command.trim();
 			action.payload.comment = action.payload.comment.trim();
 			state.stack.push(action.payload);
@@ -183,47 +382,7 @@ const isNumber = function(command) {
 };
 
 const isInDefaultDictionary = function(command) {
-	var dictionary = {
-		"ABS" : "(n -- -n)",
-		"CONSTANT" : "( -- w)",
-		"DO" : "(limit starting_value -- )",
-		"DUP" : "(n -- n n)",
-		"DROP" : "(n -- )",
-		"ELSE" : "( -- )",
-		"FALSE" : "( -- flag)",
-		"FORGET" : "(w -- )",
-		"IF" : "(flag -- )",
-		"LOOP" : "( -- )",
-		"MAX" : "(n1 n2 -- n3)",
-		"MIN" : "(n1 n2 -- n3)",
-		"MOD" : "(n1 n2 -- n3)",
-		"NEGATE" : "(n -- -n)",
-		"NOT" : "(flag1 -- flag2)",
-		"OVER" : "(n1 n2 -- n1 n2 n1)",
-		"PICK" : "(n -- nth number)",
-		"ROT" : "(n1 n2 n3 -- n2 n3 n1)",
-		"SWAP" : "(n1 n2 -- n2 n1)",
-		"THEN" : "( -- )",
-		"TRUE" : "( -- flag)",
-		"." : "(n -- )",
-		"+" : "(n1 n2 -- n1+n2)",
-		"-" : "(n1 n2 -- n1-n2)",
-		"*" : "(n1 n2 -- n1*n2)",
-		"/" : "(n1 n2 -- n1/n2)",
-		"*/" : "(n1 n2 n3 -- (n1*n2)/n3)",
-		"*/MOD" : "(n1 n2 n3 -- (n1*n2)%n3=n4 (n1*n2)/n3=n5)",
-		"<" : "(n1 n2 -- (n1<n2)=flag)",
-		">" : "(n1 n2 -- (n1>n2)=flag)",
-		"=" : "(n1 n2 -- (n1=n2)=flag)",
-		"<>" : "(n1 n2 -- (n1!=n2)=flag)",
-		";" : "( -- w)",
-		":" : "( -- )",
-		".S" : "( -- )",
-		"(" : "( -- )",
-		")" : "( -- )"
-	};
-	
-	return isNumber(command) || isSpecialDigitCommand(command) || dictionary[command.toUpperCase()]; 
+	return isNumber(command) || isSpecialDigitCommand(command) || defaultDictionary[command.toUpperCase()];
 };
 
 const displayStackReducer = function(state=[], action) {
@@ -266,13 +425,6 @@ const searchDictionary = function(action, command, dictionary) {
 	return {...action, type: "ERROR", payload: new WordNotFoundError(command) };	
 };
 
-class CustomCommand {
-	constructor() {
-		this.comment = "";
-		this.command = "";
-	}
-}
-
 const processCommands = function(action, next) {
 	action = {...action, type: ((action.type + "") ).trim() };
 	let searchForWhiteSpace = /\s+/gi;
@@ -286,8 +438,8 @@ const processCommands = function(action, next) {
 	let commands = action.type.split(searchForWhiteSpace);
 	let isWritingNewCommand = false;
 	let isWritingNewComment = false;
-	let customCommands = [];
-	let startOfCustomCommandIndex = 0;
+	let customWords = [];
+	let startOfCustomWordIndex = 0;
 	let wasErrorThrown = false;
 	commands.forEach(function(command, index) {
 		if (wasErrorThrown === false) {
@@ -295,18 +447,18 @@ const processCommands = function(action, next) {
 				switch(command) {
 					case ":" :
 						isWritingNewCommand = true;
-						startOfCustomCommandIndex = index;
-						customCommands.push(new CustomCommand() );
+						startOfCustomWordIndex = index;
+						customWords.push(new Word("", "", "") );
 						return;
 					case ";" :
 						isWritingNewCommand = false;
-						return next({...action, type: "CREATE_NEW_COMMAND", payload: customCommands.pop() });
+						return next({...action, type: "CREATE_NEW_COMMAND", payload: customWords.pop() });
 					case "(" :
 						isWritingNewComment = true;
 						break;
 					case ")" :
 						if (isWritingNewCommand) {
-							customCommands[customCommands.length - 1].comment += " " + command;
+							customWords[customWords.length - 1].comment += " " + command;
 						}
 						isWritingNewComment = false;
 						return;
@@ -322,12 +474,12 @@ const processCommands = function(action, next) {
 			} 
 			
 			if (isWritingNewCommand) {
-				if (startOfCustomCommandIndex + 1 === index) {
-					customCommands[customCommands.length - 1].name = command.toUpperCase();
+				if (startOfCustomWordIndex + 1 === index) {
+					customWords[customWords.length - 1].name = command.toUpperCase();
 				} else if (isWritingNewComment)  {
-					customCommands[customCommands.length - 1].comment += " " + command;
+					customWords[customWords.length - 1].comment += " " + command;
 				} else {
-					customCommands[customCommands.length - 1].command += " " + command;
+					customWords[customWords.length - 1].command += " " + command;
 				}
 			}
 		}
