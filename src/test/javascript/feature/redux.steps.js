4ba5ec9b15d61e0629d5fdf47fb9959a05a0e4bd
Timothy Gross <mitcoding@gmail.com>
2020-01-17 16:17:19 -0800

    cleaning up test code to make more intention revealing

--- src/test/javascript/feature/redux.steps.js
+++ src/test/javascript/feature/redux.steps.js
@@ -1,33 +1,35 @@
 import { store } from "../../../main/javascript/com/mitProductions/forth/kernel.js"
 
-function escapeForJSON(arrayJson) {
-	arrayJson = arrayJson.replace(/([,\s]*)?true([,\s]*)/gi, "$1true$2").replace(/([,\s]*)false([,\s]*)/gi, "$1false$2").replace("\r", "\\r");
-	return arrayJson;
+function convertAllStringNumbersToNumber(array) {
+	return array.map(parseStringToNumber);
 }
 
-function convertStringNumberToNumber(array) {
-	array = array.map((item) => {
-		switch (true) {
-			case (/^FALSE$/i).test(item):
-				return 0;
-			case (/^TRUE$/i).test(item):
-				return -1;
-			case /^[-]?\d+$/.test(item):
-				return item * 1;
-		}
-		return item;
+function parseStringToArray(string) {
+	string = string.replace(/([,\s]*)?true([,\s]*)/gi, "$1true$2").replace(/([,\s]*)false([,\s]*)/gi, "$1false$2");
+	return JSON.parse(string, (key, value) => {
+		return parseStringToNumber(value);
 	});
-	return array;
 }
+
+function parseStringToNumber(value) {
+	switch (true) {
+		case Array.isArray(value) :
+			return value;
+		case (/^FALSE$/i).test(value):
+			return 0;
+		case (/^TRUE$/i).test(value):
+			return -1;
+		case /^[-]?\d+$/.test(value):
+			return value * 1;
+	}
+
+	return value;
+};
 
 defineParameterType({
 	name: "array", 
 	regexp: /(\[.*\])/,
-	transformer: function(arrayJson) {
-		arrayJson = escapeForJSON(arrayJson);
-		
-		return convertStringNumberToNumber(JSON.parse(arrayJson) );
-	}
+	transformer: parseStringToArray
 });
 
 Before(function() {
@@ -51,7 +53,7 @@ Then('{string} should equal {array}', function (stackName, expectedArray) {
 	stackName = stackName.substring(0,1).toLowerCase() + stackName.substring(1);
 
     let stack = store.getState()[stackName].slice();
-	stack = convertStringNumberToNumber(stack);
+	stack = convertAllStringNumbersToNumber(stack);
 	
 	stack.should.eql(expectedArray);
 });
